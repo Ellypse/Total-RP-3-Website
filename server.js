7acee4387b9581d735770ec30c7e0f27a17f01d5
Renaud Parize <renaudparize@me.com>
2015-05-13 14:12:25 +0200

    Server + label
    Signed-off-by: Renaud Parize <renaudparize@me.com>

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.set('port', port);
  * Create HTTP server.
  */
 
-var server = http.createServer(options, app);
+var server = http.createServer(app);
 
 /**
  * Listen on provided port, on all network interfaces.
