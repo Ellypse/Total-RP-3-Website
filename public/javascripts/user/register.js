de9b4cd7d6e34f0bab443e460817c416ae1b4c44
Renaud Parize <renaud@parize.me>
2015-06-03 15:23:42 +0200

    Comments and code cleaning

--- public/javascripts/user/register.js
+++ public/javascripts/user/register.js
@@ -5,43 +5,36 @@ angular.module("register", ['mgo-angular-wizard'])
 		$scope.characters = [];
 		$scope.user = {};
 
-		$scope.finished = function() {
-			alert("Wizard finished :)");
-		};
-
-		$scope.logStep = function() {
-			console.log("Step continued");
-		};
-
-		$scope.goBack = function() {
-			WizardHandler.wizard().goTo(0);
-		};
-
 		$scope.goToCustomUsername = function(){
 			WizardHandler.wizard().goTo("username");
 		};
 
+		// Skip the username setup
 		$scope.goToEmail = function(){
+			// Set the username to the battletag of the user
 			$scope.user.username = battletag;
+			// Go to the email setting step
 			WizardHandler.wizard().goTo("email");
 		};
 
+		// Submit the data to the server
 		$scope.submit= function(){
 			$http.post("register", $scope.user)
 				.success(function(){
+					// Once the server has validated the request, redirect the user to the given destination
 					$window.location.href = destination;
 				});
 		};
 
+		// Set the character name to the one provided by the button and go to the email setting step
 		$scope.pickCharacterName = function(characterName){
 			$scope.user.username = characterName;
 			WizardHandler.wizard().goTo("email");
 		};
 
+		// On page load, request the list of characters for the current user to the server
 		$http.get("/character").success(function(data){
+			// Set the scope characters to the result of the request
 			$scope.characters = data.characters;
-			console.log($scope.characters);
-		})
-
-		$scope.loaded = false;
+		});
 	});
--- routes/character.js
+++ routes/character.js
@@ -4,37 +4,44 @@
 var appRoot = require('app-root-path'),
 	express = require('express'),
 	router = express.Router(),
-	request = require('request-promise')
-Q = require('q');
+	request = require('request-promise'),
+	Q = require('q');
 
 /**
  * Controllers
  */
 var isAuthenticated = require(appRoot + "/controllers/authentication/passport-auth").isAuthenticated;
 
-function getCharacters(region, charactersUri){
-	return request("https://" + region + charactersUri);
-}
-
+/**
+ * GET / Returns the list of characters for the current user using the Battle.net API
+ */
 router.get('/', isAuthenticated, function(req, res){
+	// List of available regions
 	var availableRegions = ['eu', 'us', 'kr', 'tw'];
+	// API url for getting the characters list
 	var charactersUri = ".api.battle.net/wow/user/characters?access_token=" + req.user.battlenet.accessToken;
 	var characters = [];
+	// The array of promises allows us to execute asynchronous request and wait for all of them to resolve to deal with the result
 	var the_promises = [];
+
 	availableRegions.forEach(function(region){
+		// Create a new promise
 		var deferred = Q.defer();
-		getCharacters(region, charactersUri).then(function(data){
+		// Request the API
+		request("https://" + region + charactersUri).then(function(data){
+			// Resolve the promise once we get the result of the request
 			deferred.resolve(JSON.parse(data).characters);
 		});
+		// Add the promise to the array of promises
 		the_promises.push(deferred.promise);
 	});
-	Q.all(the_promises).then(
-		function(data){
-			var merged = [];
-			merged = merged.concat.apply(merged, data);
-			res.json({characters: merged});
-		}
-	);
+	// Once all promises are resolved
+	Q.all(the_promises).then(function(data){
+		var merged = [];
+		// Merge each array returned by the promises into a single one array of characters data
+		merged = merged.concat.apply(merged, data);
+		res.json({characters: merged});
+	});
 });
 
 module.exports = router;
