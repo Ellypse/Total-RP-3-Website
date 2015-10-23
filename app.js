e8663e56ac8dd4b19a90073eb40871c305511801
Renaud Parize <renaud@parize.me>
2015-10-23 14:50:47 +0200

    Fonts update

--- app.js
+++ app.js
@@ -66,7 +66,7 @@ if (app.get('env') === 'development') {
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function (err, req, res, next) {
+app.use(function (err, req, res) {
 	res.status(err.status || 500);
 	res.render('error', {
 		message: err.message,
