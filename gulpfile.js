29ee8d039c5e1078473689f3794839c404de159d
Renaud Parize <renaud@parize.me>
2017-06-28 15:02:34 +0200

    Migrated stylesheets from Less to Sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,23 @@
+'use strict';
+
 var gulp = require('gulp');
 var bower = require('gulp-bower');
+var sass = require("gulp-sass");
+var autoprefixer = require("gulp-autoprefixer");
+
+gulp.task("sass", function () {
+	return gulp.src("./public/stylesheets/*.scss")
+		.pipe(sass({ outputStyle: "expanded" }).on("error", sass.logError))
+		.pipe(autoprefixer({
+			browsers: ["last 2 versions", "IE 10", "> 5%"],
+			cascade: false
+		}))
+		.pipe(gulp.dest("./public/stylesheets/"));
+});
+
+gulp.task("sass:watch", ["sass"], function () {
+	gulp.watch("./public/stylesheets/*.scss", ["sass"]);
+});
 
 gulp.task('bower', function () {
 	return bower()
